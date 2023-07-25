603a7de272e342ed8e49ed4dfa5235a1a5b85fbc
Nenners <stugawin@finance.gov.ph>
2023-07-25 14:22:07 +0800

    password account

--- Leave_Form/resources/views/approval/employeeAccountApproval.blade.php
+++ Leave_Form/resources/views/approval/employeeAccountApproval.blade.php
@@ -107,7 +107,7 @@
         <div class="row">
             <div class="form-group col-3">  
                 <label for="requested_by" class="form-label">Password</label>
-                <input type="text" class="form-control" id="password" name="password" require>
+                <input type="text" class="form-control" id="password" name="password" required>
             </div>
 
         </div>
