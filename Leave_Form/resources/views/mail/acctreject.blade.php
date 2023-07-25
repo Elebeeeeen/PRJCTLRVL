dd63f1efd689cfe97bef9f068ba23727d3817daf
Nenners <stugawin@finance.gov.ph>
2023-07-25 09:46:24 +0800

    email

--- Leave_Form/resources/views/mail/acctreject.blade.php
+++ Leave_Form/resources/views/mail/acctreject.blade.php
@@ -2,7 +2,9 @@
 
 <br>
 
-<p>I would like to inform you that your account has been <strong style="font-style: italic">Rejected.</strong></p>
+<p>We hope this email finds you well. We are writing to inform you about the status of your account application. 
+    After careful review and consideration, We regret to inform you that your account application, unfortunately, did not meet our current eligibility criteria. As a result, we have had to <strong style="font-style: italic">Reject.</strong> your application at this time. 
+    We understand that this news may be disappointing, and we sincerely apologize for any inconvenience caused. </p>
 <br>
 <p><strong>Reason:</strong> {{$reason}}</p>
 
