1cbcada6d12b987d5211d14227c485e651506246
Elebeeeen <llabingisa@finance.gov.ph>
2023-07-26 11:05:10 +0800

    for dir

--- Leave_Form/resources/views/approval/dirLeaveApproval.blade.php
+++ Leave_Form/resources/views/approval/dirLeaveApproval.blade.php
@@ -301,10 +301,12 @@
         //radio button for commutation
         if ("{{$lf_employee['commutation']}}" == $('#radio11').val()) {
             $("#radio11").prop("checked", true);
-        } else {
+        } else if ("{{$lf_employee['commutation']}}" == $('#radio12').val()) {
             $("#radio12").prop("checked", true);
+        }else {
+            
         }
-
+        
         // Calling new variable to determine array accordingly (type of leave)
         let vacation_form = $($('.leaveOption div')[0]);
         let sick_form = $($('.leaveOption div')[3]);
